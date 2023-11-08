b7c0a96de42396a18560471869681a5205dee0fb
mulitate4 <aryan.dongre1605@gmail.com>
2023-11-08 17:14:54 +0530

    React Router Dom package added Added Login page Added folder for components

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import App from "./pages/App";
+import Login from "./pages/Login";
 
 const SiteRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </BrowserRouter>
   )
