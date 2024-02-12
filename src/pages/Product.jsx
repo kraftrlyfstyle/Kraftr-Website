a915fdcce5cbc873ad521a7e2e5b62e1489f8d70
Soham Bhattacharyya <soham.b630@gmail.com>
2024-02-12 22:51:33 +0530

    Update Product.jsx

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Footer from '../components/Footer'; 
+import Footer from '../components/Footer';
 import { MdShoppingCart, MdMenu } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
@@ -16,8 +16,8 @@ const Product = () => {
   async function getShoeInfoLocal() {
     let [success, responseOrError] = await db.getShoesById(params.id);
     setLoading(false);
-    
-    if(!success){
+
+    if (!success) {
       setError(true);
       return;
     }
@@ -29,24 +29,33 @@ const Product = () => {
   useEffect(() => {
     getShoeInfoLocal();
   }, [])
-  
+
 
   return (
     <>
 
-    <div className='flex flex-row'>
-      
-      <div className="flex basis-1/3 justify-center items-center h-full">
-      <img className="rounded-md" src={"http://127.0.0.1:8000" + shoe.cover_image} alt="" />
+      <div className='flex flex-row'>
+
+        <div className="flex basis-1/3 justify-center items-center h-full bg-[#af9e97]">
+          {/*<img className="rounded-md" src={"http://127.0.0.1:8000" + shoe.cover_image} alt="" /> test with bag image first*/}
+          <img src="https://i.postimg.cc/QC03nyjd/bag-2.png" className="h-full w-full" />
+        </div>
+
+
+        <div className="flex basis-2/3  bg-[#F4ECE9] px-11 py-7 flex-col">
+          <div className="flex flex-col">
+            <Link to={"/"} className="text-4xl tracking-widest font-bold my-5 mx-10">
+              KRAFTR
+            </Link>
+            <div className="flex justify-center">
+              <p className="text text-4xl">Kraftr Bag Tops 1</p>
+            </div>
+          </div>
+
+        </div>
       </div>
 
-
-      <div className="flex basis-2/3  bg-[#F4ECE9]">
-        
-      </div>
-    </div>
-
-    <Footer></Footer>
+      <Footer></Footer>
 
     </>
   )
