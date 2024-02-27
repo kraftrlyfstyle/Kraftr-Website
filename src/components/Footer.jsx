55a2381356b8e2b30e3f7032a256d44436407028
Soham Bhattacharyya <soham.b630@gmail.com>
2024-02-27 13:31:52 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
 const Footer = () => {
   return (
-    <section id='Footer' className='flex flex-row py-16 w-full justify-between px-32 text bg-[#f6f3ec]'>
+    <section id='Footer' className='flex flex-row py-16 w-full justify-between px-32 bg-[#f6f3ec]'>
       <div className='flex flex-col mx-4'>
-        <p className='font-bold mb-2'>Kraftr</p>
-        <p className='text-xs'>Making a better future, one step at a time</p>
+        <p className='font-bold mb-2 tracking-widest text-xl'>KRAFTR</p>
+        <p className='text-xs text'>Making a better future, one step at a time</p>
       </div>
-      <div className='flex flex-col mx-4'>
+      <div className='flex flex-col mx-4 text'>
         <p className='font-bold mb-2'>Products</p>
         <a className='text-sm'>Kraftr Blues</a>
         <a className='text-sm'>Kraftr Reds</a>
         <a className='text-sm'>Kraftr Yellows</a>
         <a className='text-sm'>Kraftr Greens</a>
       </div>
-      <div className='flex flex-col mx-4'>
+      <div className='flex flex-col mx-4 text'>
         <p className='font-bold mb-2'>Useful Links</p>
         <a className='text-sm'>Who we are</a>
         <a className='text-sm'>What we do</a>
         <a className='text-sm'>About</a>
       </div>
-      <div className='flex flex-col mx-4 w-56'>
+      <div className='flex flex-col mx-4 w-56 text'>
         <p className='font-bold mb-2'>Contact</p>
         <p className='text-sm'>AB4, Manipal Institute of Technology, Manipal - 576104</p>
         <p className='text-sm'>contact@kraftrlyfstyle.com</p>
